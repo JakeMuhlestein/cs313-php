ad89365d649791e52471055ac629eb103cb325f3
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-08 22:14:47 -0600

    test2

--- web/submit.php
+++ web/submit.php
@@ -1,6 +1,6 @@
 <?php echo "User Name: " . $_POST["name"] . "<br>"; 
 	//echo "Email: " . "<a href=\"mailto:" . $_POST["email"]> . "\">" . $_POST["email"] . "</a><br>";
-	echo "<a href=\" $_POST["email"] \">"$_POST["email"]"</a> " ;
+	"<a href=\"www.example.com\">www.example.com</a>"
 	//echo "Major: " . $_POST["major"] . "<br>";
 	//echo "Comment: " . $_POST["comment"] . "<br>";
 	?>
