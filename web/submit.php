52133592394177634159e0d2e19437266b6c984a
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-08 21:16:47 -0600

    test

---
+++ web/submit.php
@@ -0,0 +1,18 @@
+<?php echo "User Name: " . $_POST["name"] . "<br>"; 
+	echo "Email: " . "<a href=\"mailto:" . $_POST["email"]> . "\">" . $_POST["email"] . "</a><br>";
+	echo "Major: " . $_POST["major"] . "<br>";
+	echo "Comment: " . $_POST["comment"] . "<br>";
+	?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+
+</body>
+</html>
