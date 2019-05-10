ab2896828bc1a0ac98316ad048eed78eb5c2e042
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-09 21:27:08 -0600

    Week03 added add to card button

--- web/Week03/Browse.php
+++ web/Week03/Browse.php
@@ -7,7 +7,7 @@
 
 	<h2>Store</h2>
 
-	<form>
+	<form action="ViewCart.php" method="get">
 		<table>
 			<tr>
 				<th>Item Name</th>
@@ -19,9 +19,30 @@
 				<td>Calander</td>
 				<td>12 months</td>
 				<td>$12.00</td>
-				<td><input type="text" name="qty"></ins></td>
+				<td><input type="text" name="qtyCal"></ins></td>
+				<td><input type="submit" value="Add to Cart"> </td>
 			</tr>
-
+			<tr>
+				<td>Book</td>
+				<td>Lots of pages</td>
+				<td>$15.00</td>
+				<td><input type="text" name="qtyBook"></ins></td>
+				<td><input type="submit" value="Add to Cart"> </td>
+			</tr>
+			<tr>
+				<td>Globe</td>
+				<td>It's a sphere</td>
+				<td>$36.00</td>
+				<td><input type="text" name="qtyGlobe"></ins></td>
+				<td><input type="submit" value="Add to Cart"> </td>
+			</tr>
+			<tr>
+				<td>Pencil</td>
+				<td>Writes Stuff</td>
+				<td>$1.00</td>
+				<td><input type="text" name="qtyPencil"></ins></td>
+				<td><input type="submit" value="Add to Cart"> </td>
+			</tr>						
 		</table>
 	</form>
 
