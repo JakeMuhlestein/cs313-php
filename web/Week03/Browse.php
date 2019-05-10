4977a600141c350c4f4d44cd9726f8e08feed261
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-09 20:51:52 -0600

    Week03 test

---
+++ web/Week03/Browse.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<h2>Store</h2>
+
+	<form>
+		<table>
+			<tr>
+				<th>Item Name</th>
+				<th>Brief Desc.</th>
+				<th>Price</th>
+				<th>Quantity</th>
+			</tr>
+
+		</table>
+	</form>
+
+
+</body>
+</html>
