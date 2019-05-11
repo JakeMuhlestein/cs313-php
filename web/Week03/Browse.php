6fe7ac56eb1f31677eccf311ae169a8db824c859
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-11 10:40:33 -0600

    Updating error testing

--- web/Week03/Browse.php
+++ web/Week03/Browse.php
@@ -74,7 +74,7 @@ foreach ( $_SESSION['cart'] as $ino) {
 <tr>
     <td>
 
-        Name: <?php echo $_SESSION['cart']['name'][$ino]; ?>
+        Name: <?php echo $_SESSION['cart'][0]['$ino']['name']; ?>
     </td>
     <td>
         Price: <?php echo $ino['price']; ?>
