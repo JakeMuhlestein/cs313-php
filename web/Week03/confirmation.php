4a1719fe6113d5fd1e476767e467b27d855a5d17
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-11 13:01:55 -0600

    Updating error testing

--- web/Week03/confirmation.php
+++ web/Week03/confirmation.php
@@ -18,14 +18,21 @@
   		<li><a class="active" href="checkOut.php">Checkout</a></li>
 	</ul>
 
+<h1>Order Compete!</h1><br><br>
+
 Shipping Info:<br>
 <?php
 
 	echo "Name: " . $_SESSION["custName"] . ".<br>";
 	echo "Address: " . $_SESSION["custAddress"] . ".<br>";
-	//echo "City: " . $_SESSION['custCity'] . "State: " . $_SESSION['custState'] . "Zip: " . $_SESSION['custZip'] .;
+	echo "City: " . $_SESSION['custCity'] . "State: " . $_SESSION['custState'] . "Zip: " . $_SESSION['custZip'] .;
 
 
+?>
+
+<br><br>Items Ordered:<br>
+
+<?php
 $total = 0;
 foreach ( $_SESSION['cart'] as $ino) {
 ?>
