3bcedfc4284d6fa6377062fcae6989ba41c43eb2
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-11 02:20:14 -0600

    Updating error testing

--- web/Week03/Browse.php
+++ web/Week03/Browse.php
@@ -19,7 +19,7 @@ if (isset ( $_POST ["buy"] )) {
     // Check the item is not already in the cart
     if (!in_array($_POST ["buy"], $_SESSION['cart'])) {
         // Add new item to cart
-        $_SESSION ['cart'] = $_POST[];
+        $_SESSION ['cart'] = $_POST["buy"];
     }
 }
 
@@ -47,8 +47,8 @@ if (isset ( $_POST ["buy"] )) {
 
 
 
-		<input type="hidden" id="name" name="book" value="Book">
-		<input type="hidden" id="price" name="price" value="10">
+		Name<input type="hidden" id="name" name="book" value="Book">
+		Price<input type="hidden" id="price" name="price" value="10">
 
 		<button type='submit' name='buy' value='$ino'>Buy</button>
 
