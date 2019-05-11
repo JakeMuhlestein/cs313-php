214e19a2ce531fb817e915d4a87f7eb7be98edc1
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-11 12:03:47 -0600

    Updating error testing

--- web/Week03/checkOut.php
+++ web/Week03/checkOut.php
@@ -1,4 +1,8 @@
 <?php
+
+
+session_start();
+
 if (isset($_POST['cart'])){
   header("Location: viewCart.php");
 }
---
+++ web/Week03/confirmation.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" type="text/css" href="week03.css">
+</head>
+<body>
+	<ul>
+  		<li><a href="Browse.php">Home</a></li>
+  		<li><a href="viewCart.php">Cart</a></li>
+  		<li><a class="active" href="checkOut.php">Checkout</a></li>
+	</ul>
+</body>
+</html>
