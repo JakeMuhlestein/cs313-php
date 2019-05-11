9ca2e1009f9d3732c0f56b071c6bc9077ea51a4a
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-10 19:49:21 -0600

    Week03 adding pages and nav bar

--- web/Week03/Browse.php
+++ web/Week03/Browse.php
@@ -7,14 +7,16 @@ $_SESSION['cart']=array();
 
 <html>
 <head>
+
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="week03.css">
 </head>
 <body>
 
 	<ul>
   		<li><a class="active" href="Browse.php">Home</a></li>
-  		<li><a href="viewCart.php">News</a></li>
-  		<li><a href="checkOut.php">Contact</a></li>
+  		<li><a href="viewCart.php">Cart</a></li>
+  		<li><a href="checkOut.php">Checkout</a></li>
 	</ul>
 
 
