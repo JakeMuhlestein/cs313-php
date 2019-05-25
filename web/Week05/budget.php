e063343a49241977d3c5789e83e4ba5ae2e69dd0
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-25 00:16:48 -0600

    db set up

--- web/Week05/budget.php
+++ web/Week05/budget.php
@@ -49,7 +49,7 @@ $budgetCat = "SELECT budget_name FROM budget_item";
 	$query = "SELECT * FROM transaction AS t
 		JOIN budget_item AS b
 		ON t.budget_id = b.id
-		WHERE t.budget_name = " . $budget .""; 
+		WHERE t.budget_name = $budget"; 
 
 	foreach ($db->query($query) as $row) {
     $id = $row['id'];
