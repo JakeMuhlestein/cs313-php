0312fd27cda6bf55eaadc630a906e1c4a0ec369c
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-28 23:05:20 -0600

    scripture page setup

---
+++ web/week05TA/scripture.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scriptures</title>
+</head>
+<body>
+
+	<form action="" method="post">
+		<input type="text" name="book"><br />
+		<input type="text" name="chapter"><br />
+		<input type="text" name="verse"><br />
+		<input type="textarea" name="">
+
+
+	</form>
+
+
+</body>
+</html>
