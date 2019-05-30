5eae4fe34c95d622087fdb4e05f85bf74a650d19
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-29 23:31:42 -0600

    add payment anme

--- web/Week05/budget.php
+++ web/Week05/budget.php
@@ -69,7 +69,7 @@ $budgetCat = "SELECT budget_name FROM budget_item";
     echo '<strong>' . $row['date'] . '</strong>' . '&nbsp;';
     echo '<strong>' . $row['budget_id'] . '</strong>' . ':';
     echo '<strong>' . $row['vend_id'] . '</strong>' . '&nbsp;';
-    echo '<strong>' . $row['payment_id'] . '</strong>' . '&nbsp;';
+    echo '<strong>' . $row['card_name'] . '</strong>' . '&nbsp;';
     echo '<strong>' . $row['amount'] . '</strong>' . '&nbsp;';
     echo '<br />';
     }
