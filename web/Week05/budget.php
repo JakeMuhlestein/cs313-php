444d81de88b43b284c0b176e258594943a9e6b2d
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-05-30 22:39:28 -0600

    adding table

--- web/Week05/budget.php
+++ web/Week05/budget.php
@@ -41,32 +41,29 @@ $budgetCat = "SELECT budget_name FROM budget_item";
     <input class="btn btn-success m-2" type="submit" value="Search">
 </form>
 
+
+<table>
+	<tr>
+		<th>Date</th>
+		<th>Budget</th>
+		<th>Vendor</th>
+		<th>Payment</th>
+		<th>Amount</th>
+	</tr>
+
 <?php
 	
 	$budget = $_POST['budgetCategories'];
 
-/*	$query = "SELECT * FROM transaction AS t
-		JOIN budget_item AS b
-		ON t.budget_id = b.id
-		WHERE b.budget_name = '$budget'"; 
 
-	foreach ($db->query($query) as $row) {
-    $id = $row['id'];
-    echo '<strong>' . $row['date'] . '</strong>' . '&nbsp;';
-    echo '<strong>' . $row['budget_id'] . '</strong>' . ':';
-    echo '<strong>' . $row['vend_id'] . '</strong>' . '&nbsp;';
-    echo '<strong>' . $row['payment_id'] . '</strong>' . '&nbsp;';
-    echo '<strong>' . $row['amount'] . '</strong>' . '&nbsp;';
-    echo '<br />';
-	}
-*/
-  $query = "SELECT * FROM transaction AS t
+
+  	$query = "SELECT * FROM transaction AS t
     	JOIN pay_method AS p ON t.payment_id = p.id
     	JOIN vendors v ON t.vend_id = v.id
 		JOIN budget_item AS b ON t.budget_id = b.id WHERE b.budget_name = '$budget'"; 
 
 	foreach ($db->query($query) as $row) {
-    $id = $row['id'];
+    //$id = $row['id'];
     echo '<strong>' . $row['date'] . '</strong>' . '&nbsp;';
     echo '<strong>' . $row['budget_name'] . '</strong>' . '&nbsp';
     echo '<strong>' . $row['vendor_name'] . '</strong>' . '&nbsp;';
