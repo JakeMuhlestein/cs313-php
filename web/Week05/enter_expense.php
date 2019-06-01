6ae12f861ab19ff4845d4825ed2e3973d5b756df
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-06-01 02:46:07 -0600

    added enter expense functionality

--- web/Week05/enter_expense.php
+++ web/Week05/enter_expense.php
@@ -32,10 +32,11 @@
 	echo $vendor;
 	echo $amount;
 
-	$db = get_db();
+	//$db = get_db();
 
-	$stmt = $db->prepare('INSERT INTO transaction(date, vend_id, payment_id, budget_id, amount) VALUES ($date, $vendor, $payment, $budget, $amount)');
-	$stmt->execute();
+	$query ('INSERT INTO transaction(date, vend_id, payment_id, budget_id, amount) VALUES ($date, $vendor, $payment, $budget, $amount)');
+	//$stmt->execute();
+	$result = pg_query($db, $query);
 
 	//$new_page = "transactions.php";
 
