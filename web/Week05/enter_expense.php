3a0330ce445815c114dfa085b7c30475d2d798fb
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-06-01 12:55:22 -0600

    added enter expense functionality

--- web/Week05/enter_expense.php
+++ web/Week05/enter_expense.php
@@ -3,6 +3,7 @@
 	include("dbconnection.php");
 	$db=get_db();
 
+/*
 	$budgetId = htmlspecialchars($_POST['budgetName']);
 	$query = "SELECT * FROM budget_item WHERE budget_name = '$budgetId'";
 	foreach ($db->query($query) as $row) {
@@ -20,18 +21,22 @@
 	foreach ($db->query($query) as $row) {
     	$vendor = $row['id'];
     }
-
+*/
 	$date = htmlspecialchars($_POST['date']);
+	$budget = htmlspecialchars($_POST['budget_name']);
+	$payment = htmlspecialchars($_POST['card_name']);
+	$vendor = htmlspecialchars($_POST['vendor']);
 	$amount = htmlspecialchars($_POST['amount']);
 
-	//echo $budget;
-	//echo $payment;
-	//echo $vendor;
+	echo $date;
+	echo $budget;
+	echo $payment;
+	echo $vendor;
 	echo $amount;
 
 	//require("dbconnection.php");
 	//$db = get_db();
-
+/*
 	//$query = 'INSERT INTO transaction (id, date, vend_id, payment_id, budget_id, amount) VALUES (DEFAULT, '$date', '$vendor', '$payment', '$budget', '$amount')';
 	$stmt = $db->prepare('INSERT INTO transaction(date, vend_id, payment_id, budget_id, amount) VALUES (:date, :vendor, :payment, :budget, :amount);');
 	//pg_query($db, $sql);
@@ -50,7 +55,7 @@
 
 	header("Location: $new_page");
 	die();
-
+*/
 
 
 ?>
