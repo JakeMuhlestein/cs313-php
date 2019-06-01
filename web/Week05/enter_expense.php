be069933e3c293bbe390a1c2b1fdb200c92ee69d
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-06-01 01:04:44 -0600

    added enter expense functionality

--- web/Week05/enter_expense.php
+++ web/Week05/enter_expense.php
@@ -3,7 +3,7 @@
 	include("dbconnection.php");
 	$db = get_db();
 	$budgetId = htmlspecialchars($_POST['budgetName']);
-	$budget = $db->prepare("SELECT id FROM budget_item b WHERE b.budget_name = $budgetId");
+	$budget = "SELECT id FROM budget_item b WHERE b.budget_name = $budgetId";
 	//$budget->execute();
 
 	//$res = pg_query($db, "SELECT id FROM budget_item b WHERE b.budget_name = $budgetId");
