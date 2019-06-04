e928227926ddbe779e134daf039ebfe67154abf0
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-06-03 23:47:54 -0600

    added enter expense functionality

--- web/Week05/enterExp.php
+++ web/Week05/enterExp.php
@@ -35,7 +35,7 @@ $vendor = "SELECT vendor_name FROM vendors";
 	<h1>Enter Expenses</h1> <br />
     <form class="m-3 form-group mx-auto" action="enter_expense.php" method="POST">
     Date:  
-    <input name="transDate" type="text">
+    <input name="date" type="text">
     <br /><br />
     Budget:  
     <select name="budgetName">
