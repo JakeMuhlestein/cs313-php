340fb0d514ce791781a930379b8a4f003aa1997c
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-06-05 22:34:43 -0600

    include adding new budget items

---
+++ web/Week05/addNew.php
@@ -0,0 +1,27 @@
+<?php
+
+	include("dbconnection.php");
+	$db=get_db();
+	$budget = $_POST['budgetName'];
+	$amount = $_POST['amount'];
+
+
+
+
+	$query = "INSERT INTO budget_Itemm 
+			(budget_name, budget_amount)
+			VALUES (:budget, :amount)";
+
+	$stmt = $db->prepare($query);
+
+	$stmt->bindValue(':budget', $budget);
+	$stmt->bindValue(':amount', $amount);
+	$stmt->execute();
+
+	$new_page = "createBudget.php";
+
+	header("Location: $new_page");
+	die();
+
+
+	
