baa6c13e87a3090e181c3004633627b6ee0e5a91
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-06-06 22:37:40 -0600

    budget tab shows only current month

--- web/Week05/budget.php
+++ web/Week05/budget.php
@@ -17,7 +17,7 @@ $end_date = date('Y-m-t');
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <title>Current Month Budget</title>
+  <title><?php echo date('F Y') ?>Current Month Budget</title>
 </head>
 <body >
 	<ul>
