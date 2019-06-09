b0a08a3e33474b63c303d3423f05c87b36c7c5c8
Jake Muhlestein <jacob.muhlestein@gmail.com>
2019-06-08 21:37:16 -0600

    budget tab shows only current month

--- web/Week05/updateList.php
+++ web/Week05/updateList.php
@@ -8,39 +8,42 @@
 	
 	if($_POST["pay"]) {
 
-	$payment = $_POST['payment'];
+		$payment = $_POST['payment'];
 
-	$query = "INSERT INTO pay_method
+		echo $payment;
+
+		$query = "INSERT INTO pay_method
 			(card_name)
 			VALUES (:payment)";
 
-	$stmt = $db->prepare($query);
+		$stmt = $db->prepare($query);
 
-	$stmt->bindValue(':payment', $payment);
+		$stmt->bindValue(':payment', $payment);
 
-	$stmt->execute();
+		$stmt->execute();
 
-	$new_page = "update.php";
+		//$new_page = "update.php";
 
-	header("Location: $new_page");
-	die();
+		//header("Location: $new_page");
+		//die();
 
 	}
 		
 	if($_POST["vend"]) {
 	
-	$vendor = $_POST['vendor'];
+		$vendor = $_POST['vendor'];
 
-	echo $vendor;
-	$query = "INSERT INTO vendors 
+		echo $vendor;
+
+		$query = "INSERT INTO vendors 
 				(vendor_name)
 			VALUES (:vendor)";
 
-	$stmt = $db->prepare($query);
+		$stmt = $db->prepare($query);
 
-	$stmt->bindValue(':vendor', $vendor);
+		$stmt->bindValue(':vendor', $vendor);
 
-	$stmt->execute();
+		$stmt->execute();
 
 	//$new_page = "update.php";
 
